120abca31539ff5a8043c0f42b85efbf06e43044
Your Name <you@example.com>
2025-04-30 10:45:34 +0530

    hello world🔥

---
+++ helloworld/target/debug/deps/helloworld.d
@@ -0,0 +1,5 @@
+C:\Users\admin\Desktop\rust-learning\helloworld\target\debug\deps\helloworld.exe: src\main.rs
+
+C:\Users\admin\Desktop\rust-learning\helloworld\target\debug\deps\helloworld.d: src\main.rs
+
+src\main.rs:
---
+++ helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+C:\Users\admin\Desktop\rust-learning\helloworld\target\debug\helloworld.exe: C:\Users\admin\Desktop\rust-learning\helloworld\src\main.rs
